37c5f439169dfe9c8262d1ede811356503656a47
hhpjjpy <1192241313@qq.com>
2016-05-31 19:07:26 -0800

    #框架插件为没一个接口添加3个基本方法

---
+++ include/Hplugin.h
@@ -0,0 +1,58 @@
+#ifndef HPLUGIN_H
+#define HPLUGIN_H
+
+struct  ReqHead {
+	int   dwSockfd;
+	int   dwPkgLen;
+	long  dwSeqno;
+	long  lReqTimeStamp;
+	long  lRecvTimeStamp;
+};
+
+template < uint32_t _iCmd, typename IN, typename OUT = NilType >
+struct Protocol
+{
+	static const uint32_t iCmd = _iCmd;
+	typedef IN in;
+	typedef OUT out;
+};
+
+#define HHP_DEFINE(CMD,PROTOCOL,_CLASS,MOTHOED) \
+extern "C" { \
+		hhp::plugin::CPlugin * make_##CMD(){return new _CLASS(); }\
+		void destory_##CMD(hhp::plugin::CPlugin* p ){if(p!=NULL) delete p; }\
+		int  servant_##CMD(hhp::plugin::CPligin* p ,const char * sReq,int reqLen,char * sResq,int MaxLen){ \
+			int iRet = 0;\
+			PROTOCOL::in  req;\
+			PROTOCOL::out resp;\
+			boss::serialize::Parser cParser(szReq,reqLen);\
+			cParser.setversion(0);\
+			try { cParser>>req; }\
+			catch ( ... ) {iRet =  0xEFFFFFFF;}\
+		if(iRet == 0) {\
+			iRet = ((_CLASS*)(p))->MOTHOED(req,resp);\
+			if(iRet<0){  } \
+			boss::serialize::Buffer cBuf;\
+			cBuf<<resp;\
+			memcpy(szResp,cBuf.getbuf(),cBuf.length());}\  
+			return iRet;  }\			
+}
+
+
+namespace hhp {
+	namespace plugin {
+		class CPlugin;
+
+		typedef CPlugin * maker_t();
+		typedef void destroy_t(CPlugin *);
+		typedef int servant_t(CPlugin *, const char *, int, char *, int);
+
+		class CPlugin{
+			//一些全局配置可在此初始化处理
+		};
+	}
+}
+
+
+#endif // !HPLUGIN_H
+
